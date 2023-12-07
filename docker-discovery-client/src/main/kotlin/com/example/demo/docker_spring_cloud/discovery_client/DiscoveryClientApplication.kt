bd7a8dbaab60b10e7fc3e66a42e7f3551302de7b
chagchagchag <chagchagchag.dev@gmail.com>
2023-12-08 01:54:38 +0900

    2023.12

--- docker-discovery-client/src/main/kotlin/com/example/demo/docker_spring_cloud/discovery_client/DiscoveryClientApplication.kt
+++ docker-discovery-client/src/main/kotlin/com/example/demo/docker_spring_cloud/discovery_client/DiscoveryClientApplication.kt
@@ -2,7 +2,9 @@ package com.example.demo.docker_spring_cloud.discovery_client
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
+@EnableDiscoveryClient
 @SpringBootApplication
 class DiscoveryClientApplication
 
