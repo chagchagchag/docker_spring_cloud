54b21ab380b01edeed09c19e121a096f57672098
chagchagchag <chagchagchag.dev@gmail.com>
2023-12-08 03:09:54 +0900

    2023.12

--- docker-discovery-service/build.gradle.kts
+++ docker-discovery-service/build.gradle.kts
@@ -73,12 +73,12 @@ jib {
         jvmFlags = listOf(
             "-Dspring.profiles.active=${profile}",
             "-XX:+UseContainerSupport",
-            "-Dserver.port=8761",
+            "-Dserver.port=9961",
             "-Dfile.encoding=UTF-8",
         )
 
         // 컨테이너 입장에서 외부로 노출할 포트
-        ports = listOf("8761")
+        ports = listOf("9961")
 
         labels = mapOf(
             "maintainer" to "chagachagchag.dev@gmail.com"
