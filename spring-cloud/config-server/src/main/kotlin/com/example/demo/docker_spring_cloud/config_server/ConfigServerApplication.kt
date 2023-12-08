fc66be3577ca94a0d203b38d65791e27d2533071
chagchagchag <chagchagchag.dev@gmail.com>
2023-12-08 20:55:54 +0900

    2023.12

--- spring-cloud/config-server/src/main/kotlin/com/example/demo/docker_spring_cloud/config_server/ConfigServerApplication.kt
+++ spring-cloud/config-server/src/main/kotlin/com/example/demo/docker_spring_cloud/config_server/ConfigServerApplication.kt
@@ -2,9 +2,10 @@ package com.example.demo.docker_spring_cloud.config_server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.config.server.EnableConfigServer
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 class ConfigServiceApplication
