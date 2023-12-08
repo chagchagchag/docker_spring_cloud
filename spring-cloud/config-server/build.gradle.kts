29dd5cb6d977eec7d64db341152ae304b2c53652
chagchagchag <chagchagchag.dev@gmail.com>
2023-12-08 21:24:37 +0900

    2023.12

--- spring-cloud/config-server/build.gradle.kts
+++ spring-cloud/config-server/build.gradle.kts
@@ -25,7 +25,7 @@ extra["springCloudVersion"] = "2022.0.4"
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.cloud:spring-cloud-config-server")
-//    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
