f90ca40ea0b06a6b064d0b0b502800e4a81d61cd
chagchagchag <chagchagchag.dev@gmail.com>
2023-12-14 05:34:42 +0900

    2023.12

--- foobar-user-api/foobar-user-service/build.gradle.kts
+++ foobar-user-api/foobar-user-service/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("de.codecentric:spring-boot-admin-starter-client:3.1.8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
---
+++ push-all.sh
@@ -0,0 +1,11 @@
+echo "git add ."
+git add .
+echo ""
+
+echo "git commit -m '2023.12'"
+git commit -m "2023.12"
+echo ""
+
+echo "git push -u origin main"
+git push -u origin main
+echo ""
