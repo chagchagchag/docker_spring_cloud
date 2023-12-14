7b753ccf659409bfcde029f2be77477ffeb5d058
chagchagchag <chagchagchag.dev@gmail.com>
2023-12-14 23:19:49 +0900

    2023.12

--- foobar-user-api/foobar-user-service/src/main/kotlin/com/example/demo/docker_spring_cloud/user_service/application/FoobarUserController.kt
+++ foobar-user-api/foobar-user-service/src/main/kotlin/com/example/demo/docker_spring_cloud/user_service/application/FoobarUserController.kt
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/")
 class FoobarUserController (
     val createUserResponseFactory: CreateUserResponseFactory,
     val readUserResponseFactory: ReadUserResponseFactory,
